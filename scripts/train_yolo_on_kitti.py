fcc951659c0019d6fa2525148788cacb1fc8fc68
Tomasz Rybotycki <rybotycki.tomasz+git@gmail.com>
2025-09-28 23:24:54 +0200

    feat(yolo): Make training function more general. Add initial version of lidar KITTI training to the script.

--- scripts/train_yolo_on_kitti.py
+++ scripts/train_yolo_on_kitti.py
@@ -1,31 +1,47 @@
 """
-
+Scripts for running YOLO training on preprepared KITTI data.
 """
 
 from ultralytics import YOLO
 
 
-def train_on_camera() -> None:
+def train_yolo_on_kitti(yaml_path: str, model_name: str, n_epochs:int = 10) -> None:
     """
+    YOLO training on preprepared KITTI data.
 
+    :note:
+        See ./scripts/prepare_kitti_for_yolo.py for info about data preparation.
     """
+    n_imgsz: int = 1248 # TODO TR: Seems to be the image width.
+
+    print(f"Start {model_name} training using {yaml_path}.")
+
     model: YOLO = YOLO("./models/yolov8n.pt")
-
-    print("Start YOLO training on KITTI camera data.")
-
     results = model.train(
-        # data="/home/piokal/automotive-tracking/datasets/KITTI_for_YOLO/KITTI.yaml",
-        data = "./datasets/YOLO_KITTI/yolo_kitti.yaml",
-        epochs=10,
-        imgsz=1248,
+        data = yaml_path,
+        epochs=n_epochs,
+        imgsz=n_imgsz,
         project="../output/",
-        name="yolov8n_trained_on_camera_1_KITTI",
+        name=model_name,
     )
 
     print("Training done.")
 
 
+
 if __name__ == "__main__":
     print("Start")
-    train_on_camera()
+
+    # Train YOLO on KITTI camera 2.
+    train_yolo_on_kitti(
+        yaml_path="./datasets/YOLO_KITTI/yolo_kitti.yaml",
+        model_name="yolov8n_trained_on_camera_2_KITTI"
+    )
+
+    # Train YOLO on KITTI lidar.
+    train_yolo_on_kitti(
+        yaml_path="./datasets/YOLO_KITTI/yolo_kitti_lidar.yaml",
+        model_name="yolov8n_trained_on_lidar_KITTI"
+    )
+
     print("Done")
